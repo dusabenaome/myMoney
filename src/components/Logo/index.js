638fd8ada63fda2f4283c6ee1b7babf1f965ed52
patrick_ndizeye <116964464+ndizeye-patrick@users.noreply.github.com>
2024-06-16 13:44:48 +0200

    all

---
+++ src/components/Logo/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../Header/images.png';
+import './index.css';
+
+const Logo = () => (
+  <div className="logo">
+    <img src={logo} alt="myMoney" />
+  </div>
+);
+
+export default Logo;
---
+++ src/containers/InitialSetup/index.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Button, Divider, Header } from 'semantic-ui-react';
+import CurrencyInput from '../Settings/Currency/Input';
+import CurrencyExchangeRate from '../Settings/Currency/ExchangeRate';
+import AccountForm from '../Accounts/Form';
+import AccountList from '../Accounts/List';
+import { completeSetup } from '../../actions/settings';
+import { loadAccounts } from '../../actions/entities/accounts';
+import { getAccountsList } from '../../selectors/entities/accounts';
+import { isSignedIn } from 'features/user/state/User.selector';
+import LoginButton from 'components/LoginButton';
+
+class InitialSetup extends React.Component {
+  componentDidMount() {
+    this.props.loadAccounts();
+  }
+
+  render() {
+    return (
+      <div className='container-flex'>
+      <div className="container-raised-desktop">
+        <Header as="h2" icon="money bill alternate" content="MyMONEY" />
+        <Divider />
+        {!this.props.isAuthenticated && (
+        <center>
+          <p>
+            <LoginButton /> 
+          </p>
+        </center>
+        )}
+       
+        
+        {this.props.accounts.length > 0 && (
+          <div style={{ margin: '1em' }}>
+            <AccountList />
+            <div className="form-submit">
+              <Button
+                primary
+                content="Finish"
+                onClick={this.props.completeSetup}
+              />
+            </div>
+          </div>
+        )}
+      </div>
+      </div>
+    );
+  }
+}
+
+InitialSetup.propTypes = {
+  isAuthenticated: PropTypes.bool,
+  accounts: PropTypes.arrayOf(PropTypes.object),
+  loadAccounts: PropTypes.func,
+  completeSetup: PropTypes.func
+};
+
+const mapStateToProps = state => ({
+  isAuthenticated: isSignedIn(state),
+  accounts: getAccountsList(state)
+});
+
+export default connect(
+  mapStateToProps,
+  { loadAccounts, completeSetup }
+)(InitialSetup);
